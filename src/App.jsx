2376bbddd64bed1acc37fd885e7cbac325bb9c6d
Mahantesh <mantu.bhavi@gmail.com>
2026-09-13 23:57:58 +0530

    updated nav and yml file

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <BrowserRouter basename="/">
         <TopNav />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/focus" element={<Focus />} />
           <Route path="/projects" element={<Projects />} />
