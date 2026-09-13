539f7ca7d54f1c6d56dc2257c8aa94c35d019711
Mahantesh <mantu.bhavi@gmail.com>
2026-09-13 23:23:32 +0530

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
-  base: "/mantubhavi/",
+  base: "/",
   server: {
     port: "3000",
   },
